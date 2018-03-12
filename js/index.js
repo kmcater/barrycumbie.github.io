fed285dc82cb7c91d9e16d4061c7ccd5df9d2ba6
kmcater <37086912+kmcater@users.noreply.github.com>
2018-03-12 15:34:32 -0500

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,39 @@
+//my js file...a whole buncha demo stuff. 
+
+$(document).ready(function(){ //this is a standard 1st line in all js files, in jQ format. this makes sure the html file is done before any scripting occurs
+//this above is self-calling f/n
+
+    $("button").click(function() {
+		console.log('clicked'); //adds event to EVERY button to add msg to console when clicked
+        $("p").toggle(); //hides and shows it. 
+		console.log('clicked'); 
+}); 
+
+//print the dom: 
+var all = document.getElementsByTagName("*");
+
+for (var i=0, max=all.length; i < max; i++) {
+     // Do something with the element here
+}
+
+var allDOMElements = document; //loade the DOCUMENT into a var 
+console.log(allDOMElements); 
+
+//document.onload = doEverything();
+
+console.log('loaded and everything is done'); 
+
+
+	console.log("...f/n called...");
+
+		document.querySelector('h1').innerHTML = "Everything Done!"; 
+		$('h1').text("Now I'm doing jQuery!").addClass("blue");
+
+		var elDoc = document;
+		console.log(elDoc);
+
+
+
+});
+
+
